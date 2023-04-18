07a681c69e28d4cf84b72cda6ab9928a1035ba6e
Francisco Orona <oronafrankie87@gmail.com>
2023-04-17 18:36:00 -0600

    forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 localStorage.clear();
 
-//function to search city name by city name entered from the apiURL.
+//function to search city name by city name entered from the apiURL along with current weather.
 function citySearch() {
   var cityName = titleCase($("#cityName")[0].value.trim());
 
@@ -47,4 +47,66 @@ function citySearch() {
       //else the city is not found this alert comes up
     }
   });
+}
+
+//City coordinates function
+
+function CityList(coordinates) {
+  apiURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    coordinates[0] +
+    "&lon=" +
+    coordinates[1] +
+    "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+
+  fetch(apiURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        getCurrentWeather(data);
+      });
+    }
+  });
+}
+//Current weather function
+
+function getCurrentWeather(data) {
+  $(".results-panel").addClass("visible");
+
+  $("#currentIcon")[0].src =
+    "http://openweathermap.org/img/wn/" +
+    data.current.weather[0].icon +
+    "@2x.png";
+  $("#temperature")[0].textContent =
+    "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
+  $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
+  $("#wind-speed")[0].textContent =
+    "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
+  $("#uv-index")[0].textContent = "  " + data.current.uvi;
+
+  getForecast(data);
+}
+
+//get forecast function
+
+function getForecast(data) {
+  for (var i = 0; i < 5; i++) {
+    var forecast = {
+      date: convertUnixTime(data, i),
+      icon:
+        "http://openweathermap.org/img/wn/" +
+        data.daily[i + 1].weather[0].icon +
+        "@2x.png",
+      temp: data.daily[i + 1].temp.day.toFixed(1),
+      humidity: data.daily[i + 1].humidity,
+    };
+
+    var currentSelector = "#day-" + i;
+    $(currentSelector)[0].textContent = forecast.date;
+    currentSelector = "#img-" + i;
+    $(currentSelector)[0].src = forecast.icon;
+    currentSelector = "#temp-" + i;
+    $(currentSelector)[0].textContent = "Temp: " + forecast.temp + " \u2109";
+    currentSelector = "#hum-" + i;
+    $(currentSelector)[0].textContent = "Humidity: " + forecast.humidity + "%";
+  }
 }
